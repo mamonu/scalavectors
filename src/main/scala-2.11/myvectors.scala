f7fed3055f69445038a6fd2c0de57ce6cece8f87
mamonu <thorosm@msn.com>
2016-05-18 22:37:49 +0100

    +-*/.x operators

--- src/main/scala-2.11/myvectors.scala
+++ src/main/scala-2.11/myvectors.scala
@@ -13,12 +13,38 @@ object myvectors extends App{
 
 
 
-  class Vector2d( x: Double, val y: Double) {
+  case class Vector2d( x: Double, val y: Double) {
 
 
     override def toString(): String = "(" + x + ", " + y + ")";
 
+
+
+    // vector magnitude
     def magnitude (): Double = math.sqrt(x*x+y*y)
+
+
+
+
+    // vector/ scalar operations
+
+    def +(a: Float) = Vector2d(x + a, y + a)
+    def -(a: Float) = Vector2d(x - a, y - a)
+    def *(a: Float) = Vector2d(x * a, y * a)
+    def /(a: Float) = Vector2d(x / a, y / a)
+
+    def cross(a: Float) = Vector2d(a * y, -a * x)
+
+
+    // vector/ vector operations
+
+
+    def +(v: Vector2d) = Vector2d(x + v.x, y + v.y)
+    def -(v: Vector2d) = Vector2d(x - v.x, y - v.y)
+
+    def dot(v: Vector2d) = x * v.x + y * v.y
+    def cross(v: Vector2d) = x * v.y - y * v.x
+
 
 
   }
@@ -29,12 +55,19 @@ object myvectors extends App{
 
 
 
+  val dotted =  pt.dot(pt2)
+  val crossed = pt.cross(pt2)
+  val plussed = pt.+(pt2) + 3
 
 
-
-  println(pt)
-  println(pt2)
+  println("v1"+pt)
+  println("v2"+pt2)
   println(pt2.magnitude())
+  println(dotted)
+  println(crossed)
+  println(plussed)
+
+
 
 
 
