ea67dfca4b307a138679048847c0ae92411b5e2f
mamonu <thorosm@msn.com>
2016-05-19 14:07:38 +0100

    added lines/polygons

--- src/main/scala-2.11/myvectors.scala
+++ src/main/scala-2.11/myvectors.scala
@@ -5,25 +5,14 @@
 import scala.math._
 
 
+
 object myvectors extends App{
-
-
-
-
-
 
 
   case class Vector2d( x: Double, val y: Double) {
 
 
     override def toString(): String = "(" + x + ", " + y + ")";
-
-
-
-    // vector magnitude
-    def magnitude (): Double = math.sqrt(x*x+y*y)
-
-
 
 
     // vector/ scalar operations
@@ -38,6 +27,22 @@ object myvectors extends App{
 
     // vector/ vector operations
 
+    def distance(v: Vector2d) = {
+      val dx = x - v.x
+      val dy = y - v.y
+      sqrt(dx * dx + dy * dy)
+    }
+
+    // vector magnitude
+    def magnitude (): Double = math.sqrt(x*x+y*y)
+
+
+    def isCoincident(v: Vector2d): Boolean = {
+      distance(v) < 0.0001
+    }
+
+
+
 
     def +(v: Vector2d) = Vector2d(x + v.x, y + v.y)
     def -(v: Vector2d) = Vector2d(x - v.x, y - v.y)
@@ -47,11 +52,74 @@ object myvectors extends App{
 
 
 
+
+    def rotate(angle: Double) = {
+      val rad = toRadians(angle)
+      val sa = sin(rad)
+      val ca = cos(rad)
+      Vector2d(x * ca - y * sa, y * ca + x * sa)
+    }
+
+    def calculateRotation(v: Vector2d) = {
+      toDegrees(atan2((v.x - x), (v.y - y)))
+    }
+
+
+
+    def calculateRotation() = {
+      toDegrees(atan2((x), (y)))
+    }
+
+
+
+
+
+
+  }
+
+  case class Line(p1: Vector2d, p2: Vector2d, intensity: Double) {
+    def this(p1: Vector2d, p2: Vector2d) = this(p1, p2, 1)
+
+    def isCoincident(p: Vector2d): Boolean =
+      p1.isCoincident(p) || p2.isCoincident(p)
+
+    def isCoincident(otherLine: Line): Boolean = {
+      p1.isCoincident(otherLine.p1) && p2.isCoincident(otherLine.p2) ||
+        p1.isCoincident(otherLine.p2) && p2.isCoincident(otherLine.p1)
+    }
+
   }
 
 
-  val pt = new Vector2d(1,2)
-  val pt2 = new Vector2d(5,5)
+  class Rectangle(p1: Vector2d, p2: Vector2d) {
+    var left = min(p1.x, p2.x)
+    var top = min(p1.y, p2.y)
+    var bottom = max(p1.y, p2.y)
+    var right = max(p1.x, p2.x)
+
+    def intersects(r2: Rectangle) =
+      !(r2.left > right ||
+        r2.right < left ||
+        r2.top > bottom ||
+        r2.bottom < top)
+
+    def render() = {
+      List(new Line(Vector2d(left, top), Vector2d(right, top)),
+        new Line(Vector2d(right, top), Vector2d(right, bottom)),
+        new Line(Vector2d(right, bottom), Vector2d(left, bottom)),
+        new Line(Vector2d(left, bottom), Vector2d(left, top)))
+    }
+
+  }
+
+
+
+
+
+
+
+  var pt = new Vector2d(0,0)
+  var pt2 = new Vector2d(1,1)
 
 
 
@@ -62,18 +130,26 @@ object myvectors extends App{
 
   println("v1"+pt)
   println("v2"+pt2)
-  println(pt2.magnitude())
-  println(dotted)
+  println("magnitude of v2"+pt2.magnitude())
+  println("dot product of 2 vectors"+dotted)
   println(crossed)
   println(plussed)
 
+  val pt3=pt.rotate(90)
+
+      pt = pt.rotate(45)
+
+
+  println(pt.calculateRotation())
+  println(pt2.calculateRotation())
+  println(pt3.calculateRotation())
+
+
+val rect = new Rectangle(pt,pt2)
 
 
 
-
-
-
-
+    println(rect.render())
   //#val now = new Date()
 
   //#println(now)
@@ -82,3 +158,6 @@ object myvectors extends App{
 
 
 }
+
+
+
